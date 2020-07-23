ed0b4fdfe6ed614b44d4ac0833dcaef83f0ba83a
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-07-22 21:44:26 -0400

    sync

--- roman.py
+++ roman.py
@@ -1,15 +1,18 @@
-modlist=[ (1000,'M'),(500,'D'), (100,'C'),(50,'L'), (10,'X'),(5,'V'), (1,'I')]
+modlist=[ (1000,'M'),(900,'CM'),(500,'D'),(400,'CD'), (100,'C'),(90,'XC'),(50,'L'),(40,'XL'), (10,'X'),(9,'IX'),(5,'V'), (4,'IV'), (1,'I')]
 
 def roman(N):
 	answer=""
 	print('input',N)
 	for item in modlist:
-		print('\n>>>',item[1])
-		print(int(N/item[0]))
-		print(item[1]*int(N/item[0]),end="")
+#	print('\n>>>',item[1])
+#	print(int(N/item[0]))
+#	print(item[1]*int(N/item[0]),end="")
 		answer+=(item[1]*int(N/item[0]))
 		N=N%item[0]
 		if N==0:
 			break
 	return answer
 print('\n'+roman(3999))
+print('\n'+roman(99))
+for i in range(3999):
+	print('\n'+roman(i))
