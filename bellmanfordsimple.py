d489f22ffc2d26a37eefcefbc8ca63447e4bef90
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-09-25 22:38:12 -0400

    bellman ford with negative cycle

--- bellmanfordsimple.py
+++ bellmanfordsimple.py
@@ -23,10 +23,13 @@ class Graph:
 				iterations+=1
 				if dist[u] != float("Inf") and dist[u]+w < dist[v]:
 					dist[v] = dist[u] + w
+				print('dist',dist)
 		for (u,v,w) in self.graph:
 			#print(i,u,v,w)
 			if dist[u] != float("Inf") and dist[u]+w < dist[v]:
 				print("Graph contains negative weight cycle") 
+				dist[v] = dist[u] + w
+				print('dist',dist)
 				return
 		print('dist',dist)
 		print('iterations',iterations)
@@ -83,6 +86,21 @@ g.addEdge(2, 0, -1)
 g.addEdge(3, 2, 5) 
 g.addEdge(3, 1, 1) 
 g.addEdge(4, 3, -3) 
+# g.print()
+# sys.exit(1)
+
+# Print the solution 
+g.bellmanford(0) 
+
+g = Graph(4) 
+# print(g)
+
+g.addEdge(0, 1, 5) 
+g.addEdge(1, 3, 3) 
+g.addEdge(0, 2, 6) 
+g.addEdge(3, 1, -7) 
+g.addEdge(2, 3, -2) 
+
 # g.print()
 # sys.exit(1)
 
