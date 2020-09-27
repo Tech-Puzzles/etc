58f5f96c8c440fe91a629aecd78ec805c89071fd
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-09-27 18:59:16 -0400

    understand binary heap

--- heapify.py
+++ heapify.py
@@ -1,3 +1,30 @@
+import random
 # heapify tree of size n at subtree rooted at index i
+#arr = [ 0 ] *100
+arr = [ 0 ] *10
+for i in range(len(arr)):
+     #arr[i]=len(arr)-i
+     arr[i]=i
+random.shuffle(arr)
+def buildheap(arr):
+	for i in range(len(arr)):
+		#arr=heapify(arr[0:i+1],i+1,i)	
+		arr=heapify(arr,i+1,i)	
+
 def heapify(arr,n,i):
+	print(arr,n,i)
+	largest=i
+	l=2*i+1
+	r=2*i+2
+	if largest < n and arr[l] > arr[largest]:
+		arr[l],arr[largest]= arr[largest],arr[l]
+	if largest < n and arr[r] > arr[largest]:
+		arr[r],arr[largest]= arr[largest],arr[r]
+
+    if largest != i: 
+		heapify(arr,n,largest)
+		
+	return arr
+
+buildheap(arr)
 
