ee5f2d43107fdd45f6770b2fe043d55fbcc08484
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-09-27 19:29:01 -0400

    understand binary heap

--- heapify.py
+++ heapify.py
@@ -7,24 +7,27 @@ for i in range(len(arr)):
      arr[i]=i
 random.shuffle(arr)
 def buildheap(arr):
-	for i in range(len(arr)):
-		#arr=heapify(arr[0:i+1],i+1,i)	
-		arr=heapify(arr,i+1,i)	
+    for i in range(len(arr)):
+        #arr=heapify(arr[0:i+1],i+1,i)    
+        heapify(arr,i+1,i)    
 
 def heapify(arr,n,i):
-	print(arr,n,i)
-	largest=i
-	l=2*i+1
-	r=2*i+2
-	if largest < n and arr[l] > arr[largest]:
-		arr[l],arr[largest]= arr[largest],arr[l]
-	if largest < n and arr[r] > arr[largest]:
-		arr[r],arr[largest]= arr[largest],arr[r]
+    print(arr[0:n],n,i)
+    largest=i
+    l=2*i+1
+    r=2*i+2
+    print('root',i,'l',l,'n',n)
+    if l < n and arr[l] > arr[largest]:
+        largest=l
+    if r < n and arr[r] > arr[largest]:
+        largest=r
 
     if largest != i: 
-		heapify(arr,n,largest)
-		
-	return arr
+        print('swap')
+        arr[i],arr[largest]= arr[largest],arr[i]
+        heapify(arr,n,i)
+        
+    #return arr
 
 buildheap(arr)
 
