34fd656b0617666bf85c9ae6a50e4d09c45010d8
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-10-01 14:23:37 -0400

    add heapify3

--- heapify2.py
+++ heapify2.py
@@ -19,17 +19,17 @@ def isHeap(arr, i, n):
 
 
 def heapify(arr,i,n):
-	largest=i
-	left=i*2+1
-	right=i*2+2
-	if left < n and arr[left] > arr[largest]:
-		largest=left
-	if right < n and arr[right] > arr[largest]:
-		largest=right
-	if largest != i:
-		#swap
-		arr[i],arr[largest] = arr[largest],arr[i]
-		heapify(arr,largest,n)
+    largest=i
+    left=i*2+1
+    right=i*2+2
+    if left < n and arr[left] > arr[largest]:
+        largest=left
+    if right < n and arr[right] > arr[largest]:
+        largest=right
+    if largest != i:
+        #swap
+        arr[i],arr[largest] = arr[largest],arr[i]
+        heapify(arr,largest,n)
 
 
 #arr=[1,2,3,4]
@@ -37,32 +37,34 @@ def heapify(arr,i,n):
 # arr=[1,2,3,4,5,6,7]
 arr=[0]*10
 for i in range(len(arr)):
-	arr[i]=i
+    arr[i]=i
 random.shuffle(arr)
+arr = [ 1, 3, 5, 4, 6, 13,  10, 9, 8, 15, 17 ]
 
 def heapsort(arr):
-		for i in reversed(range(len(arr)//2)):
-			heapify(arr,i,len(arr))
+        for i in reversed(range(len(arr)//2)):
+            heapify(arr,i,len(arr))
+"""
+        for i in reversed(range(len(arr))):
+            arr[0],arr[i] = arr[i],arr[0]
+            heapify(arr,0,i)
+"""
 
-		for i in reversed(range(len(arr))):
-			arr[0],arr[i] = arr[i],arr[0]
-			heapify(arr,0,i)
+    #    print(arr)
 
-		print(arr)
-
-# heapsort(arr)
+heapsort(arr)
 
 def heapinsert(arr,i):
-	# bubble up
+    # bubble up
 
-	parent=(i-1)//2
-	while parent != 0:
-		if arr[parent] < arr[i]:
-			arr[parent] , arr[i] = arr[i] , arr[parent] 
-			i=parent
-			parent=(i-1)//2
-			
-		
+    parent=(i-1)//2
+    while i != 0:
+        if arr[parent] < arr[i]:
+            arr[parent] , arr[i] = arr[i] , arr[parent] 
+            i=parent
+            parent=(i-1)//2
+            
+        
 print('isHeap',isHeap(arr,len(arr),0))
 print(arr)
 
