0c533b2f17c83c3bac7a2d0ecd73152f0642bc3e
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-10-02 21:32:10 -0400

    add build heap

--- heapify5.py
+++ heapify5.py
@@ -1,3 +1,4 @@
+import random
 def heapify(arr,i,n):
     largest=i
     left=largest*2+1
@@ -18,17 +19,25 @@ def heapify(arr,i,n):
 
 
 arr=[4,3,5]
+arr=[0]*100
+for i in range(len(arr)):
+	arr[i]=i
+random.shuffle(arr)
 
 def heapsort(arr):
     for i in reversed(range(len(arr)//2)):
         #print('i',i)
         heapify(arr,i,len(arr))
     print('heapify',arr)
+    #for i in range(len(arr)-1):
     for i in range(len(arr)-1):
+        end=len(arr)-i-1
         print('calling sort',len(arr)-i-1)
-        #swap 0 and i
-        arr[0],arr[len(arr)-i-1] = arr[len(arr)-i-1],arr[0]
-        heapify(arr,0,len(arr)-i-1)
+        #swap 0 and end
+        #arr[0],arr[len(arr)-i-1] = arr[len(arr)-i-1],arr[0]
+        #heapify(arr,0,len(arr)-i-1)
+        arr[0],arr[end] = arr[end],arr[0]
+        heapify(arr,0,end)
 
 
 heapsort(arr)
