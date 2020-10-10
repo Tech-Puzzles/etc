22974fd430c9449238dcfcb6b53a4d1a2f567d05
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-10-10 14:25:52 -0400

    add quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,6 +1,8 @@
 # Below partition is using Hoare's algorithm.
 def partition(arr, low, high):
+  print('===partition',arr,low,high)
   pivot_value = arr[low]
+  print('pivot_value1',pivot_value)
   i = low
   j = high
 
@@ -11,9 +13,11 @@ def partition(arr, low, high):
       j -= 1
     if i < j:
       # swap arr[i], arr[j]
+      print('swap values',arr[i],arr[j])
       arr[i], arr[j] = arr[j], arr[i]
 
   arr[low] = arr[j]
+  print('pivot_value2',pivot_value,'set to index',j)
   arr[j] = pivot_value
 
   # return the pivot index
@@ -29,7 +33,9 @@ def quick_sort(a):
   quick_sort_rec(a, 0, len(a) - 1)
   
 
-a = [55, 23, 26, 2, 18, 78, 23, 8, 2, 3]
+# a = [55, 23, 26, 2, 18, 78, 23, 8, 2, 3]
+# a = [25, 23, 26, 2, 18, 78, 23, 8, 2, 3]
+a = [6,1,7,10,2]
 
 print("Before Sorting")
 print(a)
