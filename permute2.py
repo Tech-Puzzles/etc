018c65cd901a3c67703ffe7b6378c3735487be99
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-10-11 15:16:21 -0400

    add permutation tests

--- permute2.py
+++ permute2.py
@@ -2,14 +2,18 @@ def permute(A,start):
 	if start==len(A)-1:
 		print(A)
 	else:
+		print('loop',len(A)-start)
 		for i in range(start,len(A)):
 		#for i in range(len(A)):
 			# swap i and start
 			A[start],A[i]=A[i],A[start]
 			print('swap',start,i)
+			print('set',A)
 			permute(A,start+1)
 			# swap i and start
 			A[start],A[i]=A[i],A[start]
 
-permute([1,2,3,4],0)
+# permute([1,2,3],0)
+permute([1,2,3,4,5,6,7,8,9,0],0)
+# permute([1,2,3,4],0)
 
