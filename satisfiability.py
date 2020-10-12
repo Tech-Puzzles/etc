6db4a5a7278366cb0648c09caab8a67e142c2b20
ralph yozzo <ralph@brooklynmarathon.com>
2020-10-12 11:27:47 -0400

    add 2 to the n true false tree

---
+++ satisfiability.py
@@ -0,0 +1,23 @@
+def two_to_the_n(n):
+    if n == 0:
+        print()
+        return
+    print('T',end="")
+    print('F',end="")
+    for i in range(n):
+        two_to_the_n(n-1)
+
+# two_to_the_n(3)
+
+def bool_combs(n):
+    if not n:
+        return [[]]
+    result = []
+    for comb in bool_combs(n-1):
+        result.append(comb + [True])
+        result.append(comb + [False])
+    return result
+
+# print(bool_combs(1))
+set1=bool_combs(10)
+print(len(set1))
