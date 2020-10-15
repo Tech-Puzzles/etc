fcc50df2c1081dfa647996c08e85e8fc5519d0d0
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-10-15 14:24:51 -0400

    add binary search tree

--- binary_search_tree_iterative.py
+++ binary_search_tree_iterative.py
@@ -1,4 +1,4 @@
-import collections
+from collections import deque
 
 class Node:
     def __init__(self,key):
@@ -30,4 +30,71 @@ tree=None
 tree=insert(tree,10)
 tree=insert(tree,20)
 tree=insert(tree,1)
-inorder(tree)
+# inorder(tree)
+
+def inorder_iterative(root):
+  result = ""
+  if root == None:
+    return
+
+  stk = deque()
+  count = 0
+
+  while (len(stk) != 0 or root != None):
+    if root != None:
+      stk.append(root)
+      root = root.left
+      continue
+
+    result += str(stk[-1].key) + " "
+    root = stk[-1].right
+    stk.pop()
+  return str(result)
+
+# arr = [100,50,200,25,75,35]
+root = tree
+#print("\nIterative inorder Traversal= ", end="")
+#print(inorder_iterative(root))
+
+def inorder_iterative2(root):
+  result = ""
+  if root == None:
+    return
+
+  stk = deque()
+
+  while (len(stk) != 0 or root != None):
+    #print('root',root)
+    print('stack',len(stk))
+    if root != None:
+      stk.append(root)
+      root = root.left
+      continue
+
+	# visit node
+    result += str(stk[-1].key) + " "
+    root = stk[-1].right
+    stk.pop()
+  #return str(result)
+  return result
+
+
+def bst_stack(root):
+	result=""
+	if root == None:
+		return
+	stk=deque()
+
+	while len(stk) > 0 or root != None:
+		if root != None:
+			stk.append(root)
+			root = root.left
+			continue
+		# visit node
+		result += str(stk[-1].key) + " "
+		root = stk[-1].right
+		stk.pop()
+	return result
+
+# print(inorder_iterative2(root))
+print(bst_stack(root))
