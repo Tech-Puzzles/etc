187e5b2c1f55803a378ad67be1ae32b644ad400f
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-10-15 15:03:11 -0400

    add binary search tree

--- binary_search_tree_iterative.py
+++ binary_search_tree_iterative.py
@@ -6,6 +6,9 @@ class Node:
         self.right=None
         self.key=key
 
+    def __str__(self):
+        return "key:"+str(self.key)
+
 # recursive
 def insert(root,key):
     if root is None:
@@ -27,10 +30,12 @@ def inorder(root):
 
 
 tree=None
-tree=insert(tree,10)
-tree=insert(tree,20)
-tree=insert(tree,1)
+#tree=insert(tree,10)
+#tree=insert(tree,20)
+# tree=insert(tree,1)
 # inorder(tree)
+for i in reversed(range(10)):
+	tree=insert(tree,i)
 
 def inorder_iterative(root):
   result = ""
@@ -71,32 +76,44 @@ def inorder_iterative2(root):
       root = root.left
       continue
 
-	# visit node
+    # visit node
     result += str(stk[-1].key) + " "
     root = stk[-1].right
     stk.pop()
   #return str(result)
   return result
 
+def printit(text,a):
+    print("stack:"+text)
+    for i in a:
+        print(i,end=" ")
+    print()
+
 
 def bst_stack(root):
-	result=""
-	if root == None:
-		return
-	stk=deque()
+    result=""
+    if root == None:
+        return
+    stk=deque()
 
-	while len(stk) > 0 or root != None:
-		if root != None:
-			stk.append(root)
-			# visit left
-			root = root.left
-			continue
-		# visit node
-		result += str(stk[-1].key) + " "
-		# visit right
-		root = stk[-1].right
-		stk.pop()
-	return result
+    while len(stk) > 0 or root != None:
+        print('len(stk)',len(stk),'root',root)
+        if root != None:
+            stk.append(root)
+            printit("after push",stk)
+            print('VISIT LEFT')
+            # visit left
+            root = root.left
+            continue
+        # visit node
+        print('VISIT NODE',stk[-1].key)
+        result += str(stk[-1].key) + " "
+        # visit right
+        print('VISIT RIGHT')
+        root = stk[-1].right
+        stk.pop()
+        printit("after pop",stk)
+    return result
 
 # print(inorder_iterative2(root))
 print(bst_stack(root))
