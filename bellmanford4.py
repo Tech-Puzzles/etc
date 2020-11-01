c0c7bf2f3fa816f9599dc86b2e4867f33b55cd53
Ralph Yozzo <ralph@brooklynmarathon.com>
2020-11-01 08:45:35 -0500

    bellmanford4

--- bellmanford4.py
+++ bellmanford4.py
@@ -1,28 +1,22 @@
-# (u,v,w)
-# for all len(nodes)
-# relax all edges
-# maintain dist array per node
-# initialize to infinity except source node
-
 def bellmanford(S,V,E):
     dist={}
-    for v in V:
-        dist[v]=float("inf")
+    for i in V:
+        dist[i]=float("inf")
+    # distance from start to start is 0
     dist[S]=0
 
+    # avoid cycles
     for _ in range(len(V)-1):
         for u,v,w in E:
             if dist[u] != float("inf") and dist[u] + w < dist[v]:
-                # relax
-                dist[v] = dist[u] + w
+                # relax edge
+                dist[v] = dist[u] + w 
+
     for u,v,w in E:
         if dist[u] != float("inf") and dist[u] + w < dist[v]:
-            # relax
+            # relax edge
             print('negative cycle')
-            return
     return dist
-
-
 
 V=['A','B','C']
 E=[
@@ -34,6 +28,7 @@ print(bellmanford('A',V,E))
 
 print(bellmanford('s',['s','a','b','c'],[('s', 'a', 5) , ('a', 'c', 3) , ('s', 'b', 6) , ('c', 'a', -7) , ('b', 'c', -2) ]))
 print(bellmanford('s',['s','a','b','c'],[('s', 'a', 5) , ('a', 'c', 3) , ('s', 'b', 6) , ('c', 'a', 7) , ('b', 'c', -2) ]))
+
 
 
 
